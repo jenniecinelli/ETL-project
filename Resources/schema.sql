b7eb8935a166ddf3f3b684dbc189ffd2def04e95
Jenna Murphy <jennadeemurphy@gmail.com>
2021-06-12 14:34:32 -0500

    shema update

---
+++ Resources/schema.sql
@@ -0,0 +1,39 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/rQRMYp
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "billionaires" (
+    "billionaire_id" SERIAL   NOT NULL,
+    "name" VARCHAR   NOT NULL,
+    "net_worth" INT   NOT NULL,
+    "country_name" VARCHAR   NOT NULL,
+    "city" VARCHAR   NOT NULL,
+    "state" VARCHAR   NOT NULL,
+    "Status" VARCHAR   NOT NULL,
+    "Children" INT   NOT NULL,
+    "dropout" BOOLEAN   NOT NULL,
+    "School1" VARCHAR   NOT NULL,
+    "School2" VARCHAR   NOT NULL,
+    "self_made" BOOLEAN   NOT NULL,
+    CONSTRAINT "pk_billionaires" PRIMARY KEY (
+        "billionaire_id"
+     )
+);
+
+CREATE TABLE "world_bank" (
+    "country_name" VARCHAR   NOT NULL,
+    "country_code" VARCHAR   NOT NULL,
+    "region_code" VARCHAR   NOT NULL,
+    "gdp" INT   NOT NULL,
+    "population" INT   NOT NULL,
+    "population_cgr" FLOAT   NOT NULL,
+    "internet_users" INT   NOT NULL,
+    "life_expectancy" INT   NOT NULL,
+    CONSTRAINT "pk_world_bank" PRIMARY KEY (
+        "country_name"
+     )
+);
+
+ALTER TABLE "billionaires" ADD CONSTRAINT "fk_billionaires_country_name" FOREIGN KEY("country_name")
+REFERENCES "world_bank" ("country_name");
--- schema.sql
+++ schema.sql
@@ -2,37 +2,38 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/rQRMYp
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
-CREATE TABLE "billionaires" (
-    "billionaire_id" SERIAL   NOT NULL,
-    "name" VARCHAR   NOT NULL,
-    "net_worth" INT   NOT NULL,
-    "country_name" VARCHAR   NOT NULL,
-    "city" VARCHAR   NOT NULL,
-    "state" VARCHAR   NOT NULL,
-    "status" VARCHAR   NOT NULL,
-    "children" INT   NOT NULL,
-    "degree" VARCHAR   NOT NULL,
-    "university" VARCHAR   NOT NULL,
-    "self_made" BOOLEAN   NOT NULL,
-    CONSTRAINT "pk_billionaires" PRIMARY KEY (
-        "billionaire_id"
-     )
-);
-
 CREATE TABLE "world_bank" (
     "country_name" VARCHAR   NOT NULL,
     "country_code" VARCHAR   NOT NULL,
     "region_code" VARCHAR   NOT NULL,
     "gdp" INT   NOT NULL,
     "population" INT   NOT NULL,
-    "population_cgr" INT   NOT NULL,
+    "population_cgr" FLOAT   NOT NULL,
     "internet_users" INT   NOT NULL,
     "life_expectancy" INT   NOT NULL,
     CONSTRAINT "pk_world_bank" PRIMARY KEY (
         "country_name"
      )
 );
+
+CREATE TABLE "billionaires" (
+    "billionaire_id" SERIAL,
+    "name" VARCHAR,
+    "net_worth" FLOAT,
+    "country_name" VARCHAR,
+    "city" VARCHAR,
+    "state" VARCHAR,
+    "Status" VARCHAR,
+    "Children" FLOAT,
+    "dropout" BOOLEAN,
+    "School1" VARCHAR,
+    "School2" VARCHAR,
+    "self_made" BOOLEAN,
+    CONSTRAINT "pk_billionaires" PRIMARY KEY (
+        "billionaire_id"
+     )
+);
+
 
 ALTER TABLE "billionaires" ADD CONSTRAINT "fk_billionaires_country_name" FOREIGN KEY("country_name")
 REFERENCES "world_bank" ("country_name");
